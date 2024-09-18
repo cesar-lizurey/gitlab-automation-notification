ae9f7f2b5f7de67135956b25f43430f537778800
cesar-lizurey <cesar@lizurey.fr>
2024-09-18 18:47:06 +0200

    Amélioration des journaux

--- verif_version.sh
+++ verif_version.sh
@@ -33,14 +33,14 @@ LATEST_DATE_TIMESTAMP=$(date -d "$LATEST_DATE" +%s)
 # Comparer les digests
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 if [ "$ACTUAL_DIGEST" = "$LATEST_DIGEST" ]; then
-    echo "$TIMESTAMP --> Version $GITLAB_VERSION est a jour." >> $SCRIPT_DIR/maj_docker.log
+    echo "$TIMESTAMP - Version $GITLAB_VERSION est a jour." >> $SCRIPT_DIR/maj_docker.log
 else
-    echo "$TIMESTAMP --> Version $LATEST_VERSION disponible en mise à jour de $GITLAB_VERSION." >> $SCRIPT_DIR/maj_docker.log
+    echo "$TIMESTAMP - Version $LATEST_VERSION disponible en mise à jour de $GITLAB_VERSION." >> $SCRIPT_DIR/maj_docker.log
     # Nombre de jours de sécurité avant une mise à jour automatique
     DELAY=$(jq -r '.delay' $SCRIPT_DIR/verif_version.conf)
     DATE_MARGIN=$(date -d "$DELAY days ago" +%s)
     if [ $LATEST_DATE_TIMESTAMP -lt $DATE_MARGIN ]; then
-        echo "$TIMESTAMP --> Version latest est plus à jour que la version actuelle $GITLAB_VERSION. On met à jour."
+        echo "$TIMESTAMP - Version latest est plus à jour que la version actuelle $GITLAB_VERSION. On met à jour."
         # On notifie que la mise à jour vers telle version va se faire
         curl -H "Title: [$SERVER_NAME] Mise à jour Gitlab" -H "Tags: warning" -d "En cours vers $LATEST_VERSION" $NTFY_URL/maj_gitlab -u :$NTFY_TOKEN
         curl -H "Content-Type: application/json" -X POST -d "{\"content\": \":warning: **MAJ GITLAB** :warning:\\n\\nMise à jour en cours vers la version $LATEST_VERSION.\\n\\nCe processus dure environ 5 minutes, durant lesquelles le Gitlab sera inaccessible. Un nouveau  message sera publié quand la mise à jour est terminée.\"}" "$DISCORD_WEBHOOK"
@@ -49,8 +49,8 @@ else
         curl -H "Title: [$SERVER_NAME] Mise à jour Gitlab" -H "Tags: white_check_mark" -d "Version $LATEST_VERSION" $NTFY_URL/maj_gitlab -u :$NTFY_TOKEN
         curl -H "Content-Type: application/json" -X POST -d "{\"content\": \":white_check_mark: **MAJ GITLAB**\\n\\nMise à jour effectuée sur la version $LATEST_VERSION.\"}" "$DISCORD_WEBHOOK"
     else
-        echo "$TIMESTAMP --> Datant de moins de $DELAY jour(s), la mise à jour est en attente." >> $SCRIPT_DIR/maj_docker.log
+        echo "$TIMESTAMP - Datant de moins de $DELAY jour(s), la mise à jour est en attente." >> $SCRIPT_DIR/maj_docker.log
         # On notifie que la version est encore trop récente de X jours
-        curl -H "Title: [$SERVER_NAME] Mise à jour Gitlab" -H "Tags: hourglass" -d "Version $LATEST_VERSION date de moins de $DELAY jour\(s\)" $NTFY_URL/maj_gitlab -u :$NTFY_TOKEN
+        curl -H "Title: [$SERVER_NAME] Mise à jour Gitlab" -H "Tags: hourglass" -d "Version $LATEST_VERSION date de moins de $DELAY jour(s)" $NTFY_URL/maj_gitlab -u :$NTFY_TOKEN
     fi
 fi
